92ee32d32a4f54d090651c80a99592c522b4e2ff
Rafael Augusto Miranda <rafael.miranda.ext@rendimento.com.br>
2021-12-26 12:37:13 -0300

    seed database por migrations

---
+++ SeedData.Console/Migrations/20211226153345_seed-database.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeedData.Console.Migrations
+{
+    public partial class seeddatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("insert into animais (nome, idade, datacadastro) values ('animal 01', 3, getdate())");
+            migrationBuilder.Sql("insert into animais (nome, idade, datacadastro) values ('animal 02', 6, getdate())");
+            migrationBuilder.Sql("insert into animais (nome, idade, datacadastro) values ('animal 03', 9, getdate())");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
