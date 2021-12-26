37f986c35264eaf1f9ab6fdbf276886b7e9ac399
Rafael Augusto Miranda <rafael.miranda.ext@rendimento.com.br>
2021-12-26 14:16:36 -0300

    convenções tabelas

---
+++ Convencoes.Console/Data/VeterinariaContext.cs
@@ -0,0 +1,24 @@
+﻿using SeedData.Console.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Convencoes.Console.Data
+{
+    public class VeterinariaContext : DbContext
+    {
+        public VeterinariaContext()
+        {
+
+        }
+        public VeterinariaContext(DbContextOptions<VeterinariaContext> options)
+           : base(options)
+        {
+        }
+
+        public virtual DbSet<Cliente> Clientes { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SLT-002411\\SQLEXPRESS;Database=VeterinariaDb;Trusted_Connection=True");
+        }
+    }
+}
