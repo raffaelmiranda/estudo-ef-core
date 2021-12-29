dcd988f6130ca6e4e7256841452d6da45d129728
Rafael Augusto Miranda <rafael.miranda.ext@rendimento.com.br>
2021-12-28 21:12:12 -0300

    mostra sql gerado

---
+++ MostraSQL.Console/Domain/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace MostraSQL.Console.Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public int Estoque { get; set; }
+        public string? Categoria { get; set; }
+    }
+}
