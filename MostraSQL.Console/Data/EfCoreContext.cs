615833ee4729fb803142ee085b774711d06b463b
Rafael Augusto Miranda <rafael.miranda.ext@rendimento.com.br>
2021-12-29 21:43:42 -0300

    estados das entidades e mostrando o sql gerado

--- MostraSQL.Console/Data/EfCoreContext.cs
+++ MostraSQL.Console/Data/EfCoreContext.cs
@@ -1,17 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MostraSQL.Console.Domain;
+using System.Diagnostics;
 
 namespace MostraSQL.Console.Data
 {
     public class EfCoreContext : DbContext
     {
 
-
         public DbSet<Produto>? Produtos { get; set; }
+
+        private readonly StreamWriter _logStream = new StreamWriter("mylog.txt", append: true);
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=SLT-002411\\SQLEXPRESS;Database=EfCoreDb;Trusted_Connection=True");
+            
+            //Log Console
+            optionsBuilder.LogTo(System.Console.WriteLine);
+
+            //Log Janela Ouput
+            //optionsBuilder.LogTo(message => Debug.WriteLine(message));
+
+            //Log Arquivo
+            //optionsBuilder.LogTo(_logStream.WriteLine);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -38,6 +49,18 @@ namespace MostraSQL.Console.Data
                     Estoque = 20,
                     Categoria = "Material Escolar"
                 });
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _logStream.Dispose();
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await _logStream.DisposeAsync();
         }
     }
 }
--- MostraSQL.Console/Program.cs
+++ MostraSQL.Console/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using MostraSQL.Console.Data;
+using MostraSQL.Console.Domain;
+
+using (var contexto = new EfCoreContext())
+{
+    //var produtos = contexto.Produtos.Where(p => p.Estoque >= 15).ToList();
+
+    InserirProduto(contexto);
+    AtualizarProduto(contexto);
+    ExcluirProduto(contexto);
+}
+
+Console.ReadKey();
+
+void InserirProduto(EfCoreContext contexto)
+{
+    Console.WriteLine("=============== INSERINDO PRODUTO ===============");
+
+    var produtoNovo = new Produto()
+    {
+        Nome = "Produto Novo",
+        Estoque = 11,
+        Categoria = "Nova"
+    };
+
+    contexto.Add(produtoNovo);
+    ExibeEstado(contexto.ChangeTracker.Entries());
+
+    contexto.SaveChanges();
+    ExibeEstado(contexto.ChangeTracker.Entries());
+
+    Console.WriteLine(Environment.NewLine);
+}
+
+
+void AtualizarProduto(EfCoreContext contexto)
+{
+    Console.WriteLine("=============== ATUALIZANDO PRODUTO ===============");
+
+    var produtoAlterar = contexto.Produtos.First();
+    produtoAlterar.Nome = "Nome alterado";
+
+    ExibeEstado(contexto.ChangeTracker.Entries());
+
+    contexto.SaveChanges();
+    ExibeEstado(contexto.ChangeTracker.Entries());
+}
+
+void ExcluirProduto(EfCoreContext contexto)
+{
+    Console.WriteLine("=============== EXCLUINDO PRODUTO ===============");
+
+    var produtoExcluir = contexto.Produtos.First();
+    contexto.Remove(produtoExcluir);
+
+    ExibeEstado(contexto.ChangeTracker.Entries());
+
+    contexto.SaveChanges();
+    var entry = contexto.Entry(produtoExcluir);
+    Console.WriteLine($">>>>>>> ESTADO DA ENTIDADE EXCLUÍDA: {entry.State}");
+}
+
+void ExibeEstado(IEnumerable<EntityEntry> entries)
+{
+    foreach (var entrada in entries)
+    {
+        Console.WriteLine($"\n>>>>>>> ENTIDADE: {entrada.Entity.GetType().Name}, " +
+                          $"Estado: { entrada.State.ToString() }");
+    }
+}
